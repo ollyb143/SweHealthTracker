08c4b0a6b6f30d07505199ee3a3cdf47f3fb99c2
dylayoun <16dylayoun@gmail.com>
2025-03-24 14:21:58 +0000

    Adding the exercise page with the css

--- client/src/views/pages/ExercisePage.jsx
+++ client/src/views/pages/ExercisePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+import "./ExercisePage.css";
 
 
 const ExercisePage = () => {
@@ -9,6 +10,38 @@ const ExercisePage = () => {
           {/* Navbar */}
           <NavBar />
           <h1>Welcome to the Exercise Page</h1>
+
+          <div class="container">
+            <div class="header">
+              <h1>Log Exercise</h1>
+              <p>Welcome! This is where you can log your exercise for the day</p>
+            </div>
+
+            <div class="excerise-form">
+              <div class="data-time">
+                <label>Date:</label>
+                <input type="date" id="excercise-date"></input>
+
+                <label>Time:</label>
+                <input type="time" id="excercise-time"></input>
+              </div>
+
+              <div class="excercise-type">
+                <label>Excercise Type:</label>
+                <div class="buttons">
+                    <button class="type-btn">Walking</button>
+                    <button class="type-btn">Cycling</button>
+                    <button class="type-btn">Swimming</button>
+                    <button class="type-btn">Running</button>
+                    <button class="type-btn">Pilates</button>
+                    <button class="type-btn">Climbing</button>
+                </div>
+                <input type="number" id="length-input" placeholder="Enter length in minutes"></input>
+            </div>
+
+            <button id="log-btn">Log Excercise</button>
+        </div>
+    </div>
 
 
           {/* Footer */}
