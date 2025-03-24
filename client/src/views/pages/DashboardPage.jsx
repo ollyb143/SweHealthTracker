c2364e6a10732359e72d8743c944793c104d2262
Oliver Borowski <olly.borowski@gmail.com>
2025-03-24 15:44:51 +0000

    WIP: dashboard page.

--- client/src/views/pages/DashboardPage.jsx
+++ client/src/views/pages/DashboardPage.jsx
@@ -7,9 +7,28 @@ const DashboardPage = () => {
     <div>
       {/* Navbar */}
       <NavBar />
-      <h1>Welcome to the Dashboard Page</h1>
+        <main>
 
-      
+          <section id="dashboardHeader">
+            <h1>Your Dashboard</h1>
+            <p>Welcome! This is where you can track your progress for the day.</p>
+          </section>
+
+          <section id="exerciseSection">
+            <h2>Track your exercise for the day:</h2>
+            <ul id="exerciseList">
+              <li>*Exercise 1*</li>
+              <li>*Exercise 2*</li>
+              <li>*Exercise 3*</li>
+            </ul>
+          </section>
+
+          <section id="dietSection">
+            <h2>Track your diet:</h2>
+            <h3>Breakfast:</h3>
+            
+          </section>
+        </main>
       {/* Footer */}
       <Footer />
     </div>
