c032364b111d883fa573db5b6073bf5a79a23149
abiosifeso <119352175+abiosifeso@users.noreply.github.com>
2025-05-06 12:33:35 +0100

    register page correction

--- client/src/views/pages/RegisterPage.jsx
+++ client/src/views/pages/RegisterPage.jsx
@@ -155,8 +155,8 @@ const RegisterPage = () => {
                   <option value="">Select...</option>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
-                  <option value="Other">Other</option>
-                  <option value="PNTS">Prefer not to say</option>
+                  <option value="other">Other</option>
+                  <option value="prefer-not-to-say">Prefer not to say</option>
                 </select>
               </div>
 
